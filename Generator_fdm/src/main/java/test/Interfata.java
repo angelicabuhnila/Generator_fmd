06b1a4fba256ec841c829629f5a2e682ca7f869c
Denisa Ungureanu <denisa-elena.ungureanu@student.tuiasi.ro>
2023-05-11 19:22:15 +0300

    proba1

--- Generator_fdm/src/main/java/test/Interfata.java
+++ Generator_fdm/src/main/java/test/Interfata.java
@@ -3,6 +3,11 @@ package test;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Interfata {
 	
@@ -39,8 +44,32 @@ public class Interfata {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(400,400, 631, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, "name_49646790304500");
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(45, 42, 85, 21);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(45, 114, 85, 21);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(45, 193, 85, 21);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(45, 270, 85, 21);
+		panel.add(btnNewButton_3);
 	}
-
 }
