6502bf07abe33588638e34ae8d3b38a826d47264
Denisa Ungureanu <denisa-elena.ungureanu@student.tuiasi.ro>
2023-05-11 21:39:35 +0300

    var1

--- Generator_fdm/src/main/java/test/Interfata.java
+++ Generator_fdm/src/main/java/test/Interfata.java
@@ -2,12 +2,16 @@ package test;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import java.util.Scanner;
+import com.spire.doc.Document;
 
 public class Interfata {
 	
@@ -52,20 +56,51 @@ public class Interfata {
 		frame.getContentPane().add(panel, "name_49646790304500");
 		panel.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Completare date 1");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(45, 42, 85, 21);
+		btnNewButton.setBounds(45, 42, 346, 21);
 		panel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(45, 114, 85, 21);
+		btnNewButton_1.setBounds(45, 114, 306, 21);
 		panel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(45, 193, 85, 21);
+		btnNewButton_2.setBounds(45, 193, 346, 21);
 		panel.add(btnNewButton_2);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(69, 291, 5, 22);
+		panel.add(textArea);
+		
+		 btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			
+				Scanner scanner = new Scanner(System.in);
+		        System.out.print("Facultatea: ");
+		        String var0 = scanner.nextLine();
+		        System.out.print("Departament: ");
+		        String var1 = scanner.nextLine();
+		        scanner.close();
+		        
+		        Document document = new Document();
+		        document.loadFromFile("D:\\an3\\ProiectPIP\\fisa11.docx");
+		        document.setReplaceFirst(true);
+		        document.replace("_____________________", var0, false, true);
+		        document.replace("___________________", var1, false, true);
+		        document.saveToFile("Find&Replace2.docx");
+
+		        
+		        
+				
+				
+					
+					}
+				});
 	}
-}
+	}
+
--- Generator_fdm/src/main/java/test/test.java
+++ Generator_fdm/src/main/java/test/test.java
@@ -1,7 +1,14 @@
 package test;
 
+import java.util.Scanner;
+
+import com.spire.doc.Document;
+
+
+import com.spire.doc.Document;
+
 public class test {
 	
-	//proba3
+	
 
 }
