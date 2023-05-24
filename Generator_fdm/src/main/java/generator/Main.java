0ea6eaf0923e3086060838051c9a673a4590d831
Angelica <Home-pc@Angelica>
2023-05-24 11:54:43 +0300

    Main in curs de lucru.

--- Generator_fdm/src/main/java/generator/Main.java
+++ Generator_fdm/src/main/java/generator/Main.java
@@ -16,18 +16,27 @@ public class Main {
 		Interfata2 pag2 = new Interfata2();
 		String s = "Interfata1";
 		DateProfesor prof = new DateProfesor();
-		Vector<DateProfesor> profi = new Vector<DateProfesor>();
-		OperatiiNotped txt = new OperatiiNotped();
+		Vector<DateProfesor> profesori = new Vector<DateProfesor>();
+		OperatiiNotepad notePed = new OperatiiNotepad();
 		Vector<String> nume_profesori = new Vector<String>();
+		Vector<String> nume_materii = new Vector<String>();
+		Vector<String> nume_sali = new Vector<String>();
+		
 
 		while (true) {
 
 			s = pag.setInterface(s, pag1, pag2);
 			if (s == "Interfata2") {
-				String loc_prof = pag1.array[0];
-				profi = txt.readLine(loc_prof);
-				nume_profesori = prof.numeProfesori(profi);
-				pag2 = new Interfata2(nume_profesori);
+				String loc_document = pag1.array[0];
+				String loc_profesori = pag1.array[1];
+				String loc_sali=pag1.array[2];
+				String loc_grupe=pag1.array[3];
+				profesori = notePed.readLine(loc_profesori);
+				nume_profesori=prof.numeProfesori(profesori);
+				nume_materii=prof.numeMateriiFunctie(profesori, "Carlos");
+				nume_sali=notePed.readLine1(loc_sali);
+				
+				pag2 = new Interfata2(nume_profesori,nume_sali,nume_materii);
 
 				pag1.array[4] = null;
 
